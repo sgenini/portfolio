86ac1168bf8a2ae4fd4b21b7b849d074d1f981e4
Stephane Genini <sgenini@gmail.com>
2019-03-29 14:59:57 -0700

    fix projects page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {  MDBRow, MDBCol, MDBCard, MDBCardBody, MDBIcon, MDBContainer, MDBCarousel, MDBCardHeader, MDBCarouselInner, MDBCarouselItem, MDBView, MDBMask } from "mdbreact";
+import {  MDBRow, MDBCol, MDBCard, MDBCardBody, MDBIcon, MDBContainer, MDBCarousel, MDBCardHeader, MDBCardImage, MDBCarouselInner, MDBCarouselItem, MDBView, MDBMask } from "mdbreact";
 
 // CREST PHOTOS
 import crestWelcome from "./Crest/images/welcome.png";
@@ -21,6 +21,13 @@ import noshHome from "./Nosh/images/Home.png";
 import noshRecipe from "./Nosh/images/Recipe.png";
 import noshShopping from "./Nosh/images/Shopping.png";
 
+// INDIVIDUAL PHOTOS
+import bamazon from "./Individual/bamazon.gif";
+import GifTastic from "./Individual/GifTastic.png";
+import GuessThatFruit from "./Individual/GuessThatFruit.png";
+import CrazyCrystalCollectors from "./Individual/CrazyCrystalCollectors.png";
+
+
 const Projects = () => {
   return (
     <section className="projects-page mb-5" style={{opacity: ".9", paddingTop: "120px", minHeight: "90vh"}}>
@@ -29,7 +36,7 @@ const Projects = () => {
           <MDBRow>
             <MDBCol lg="12" className="mx-auto">
               <h1 className="mt-4 mb-3 text-lg-center" color="#FFFFFF"><strong><strong style={{fontFamily: "'Crimson Text', serif"}}>Development Projects</strong></strong></h1>
-              <p>Feel free to check out some of these projects I've worked on as I learned to code! <br />
+              <p className="px-4">Feel free to check out some of these projects I've worked on as I learned to code! <br />
               I'll be adding to this page periodically as I continue to develop projects.</p>
               <h1><MDBIcon className="" fab icon="codepen" /></h1>
             </MDBCol>
@@ -37,12 +44,12 @@ const Projects = () => {
         </MDBCard>
       </MDBContainer>
 
-      <MDBCol lg="11" className="mx-auto mt-5">
+      <MDBCol lg="11" className="mx-auto mt-4">
         <MDBRow>
           <MDBCol lg="6" className="mx-auto">
-            <MDBCard className="shadow-box-example hoverable">
+            <MDBCard className="shadow-box-example hoverable mb-4">
               <h1 className="mt-4 mb-3 text-lg-center" color="#FFFFFF"><strong><strong style={{fontFamily: "'Crimson Text', serif"}}>Collaborative</strong></strong></h1>
-              <p>This is a collection of work that I've developed as part of a team, group, or pair programming approach.</p>
+              <p className="px-4">This is a collection of work that I've developed as part of a team, <br /> group, or pair programming approach.</p>
               <hr style={{width: "60%"}} className="mx-auto" />
               <MDBCardBody>
                 
@@ -178,12 +185,63 @@ const Projects = () => {
               </MDBCardBody>
             </MDBCard>
           </MDBCol>
+
           <MDBCol lg="6" className="mx-auto">
             <MDBCard className="shadow-box-example hoverable">
               <h1 className="mt-4 mb-3 text-lg-center" color="#FFFFFF"><strong><strong style={{fontFamily: "'Crimson Text', serif"}}>Individual</strong></strong></h1>
-              <p>Feel free to check out some of these projects I've worked on as I learned to code! <br />
-              I'll be adding to this page periodically as I continue to develop projects.</p>
-              <h1><MDBIcon className="" fab icon="codepen" /></h1>
+              <p className="px-3">Check out some of my solo endeavors, from JavaScript browser-based <br /> games & apps to more developed web-based services!</p>
+              <hr style={{width: "60%"}} className="mx-auto" />
+            <MDBCardBody>
+              
+                {/* Bamazon */}
+                <MDBCard style={{width: "90%"}} className="mx-auto mb-4">
+                  <MDBCardHeader>
+                    <h5><strong>bAmazon<br /></strong><small>CLI-based Storefront and Inventory Management</small></h5>
+                  </MDBCardHeader>
+                  <MDBCardImage src={bamazon} className="img-fluid"/>
+                  <MDBCardBody>
+                    <p>bAmazon is a MySQL-database storefront, taking in orders from customers and depleting  stock from the store’s inventory. Additionally, we can program our app to track product sales across our store’s departments and then provide a summary of the highest-grossing departments in the store in the management and supervisor modes. Download the program and run it in a node shell.</p>
+                    <a href="https://github.com/sgenini/bamazon" target="_blank" rel="noopener noreferrer">GitHub</a>
+                  </MDBCardBody>
+                </MDBCard>
+              
+                {/* Crazy Crystal Collectors */}
+                <MDBCard style={{width: "90%"}} className="mx-auto mb-4">
+                  <MDBCardHeader>
+                    <h5><strong>Crazy Crystal Collectors<br /></strong><small>A Mathematical Matching Game</small></h5>
+                  </MDBCardHeader>
+                  <MDBCardImage src={CrazyCrystalCollectors} className="img-fluid"/>
+                  <MDBCardBody>
+                    <p>You will be assigned a Goal Number at the start of the game. Click on the Crystals below to add its specific amount of points to your Current Number. You win the game by matching your Current Number to the Goal Number chosen. You lose the game if your Current Number surpasses the Goal Number. PS- The values of the crystals are hidden. (Use arithmetic to figure out each crystal's value!) Each new round, the game will randomize the Goal Number and the values of the Crystals. Good luck, and have fun!</p>
+                    <a href="https://sgenini.github.io/CrazyCrystalCollectors/" target="_blank" rel="noopener noreferrer">Deployed</a> | <a href="https://github.com/sgenini/CrazyCrystalCollectors" target="_blank" rel="noopener noreferrer">GitHub</a>
+                  </MDBCardBody>
+                </MDBCard>
+              
+                {/* GifTastic */}
+                <MDBCard style={{width: "90%"}} className="mx-auto mb-4">
+                  <MDBCardHeader>
+                    <h5><strong>GifTastic<br /></strong><small>A Giphy API Web Application</small></h5>
+                  </MDBCardHeader>
+                  <MDBCardImage src={GifTastic} className="img-fluid"/>
+                  <MDBCardBody>
+                    <p>Tap into GIPHYs API to summon GIFs! Click the pre-generated buttons, or add your own categories! The app will return a collection of gifs, including their ratings -- simply click one to play!</p>
+                    <a href="https://sgenini.github.io/giphy-api/" target="_blank" rel="noopener noreferrer">Deployed</a> | <a href="https://github.com/sgenini/giphy-api" target="_blank" rel="noopener noreferrer">GitHub</a>
+                  </MDBCardBody>
+                </MDBCard>
+              
+                {/* Guess That Fruit */}
+                <MDBCard style={{width: "90%"}} className="mx-auto mb-4">
+                  <MDBCardHeader>
+                    <h5><strong>Guess That Fruit<br /></strong><small>A Hangman Style Browser Game</small></h5>
+                  </MDBCardHeader>
+                  <MDBCardImage src={GuessThatFruit} className="img-fluid"/>
+                  <MDBCardBody>
+                    <p>Use your keyboard to type a letter. If the letter is in the word, it will update to show where it appears in the word. If the letter is not in the word, you will lose a guess and it will appear in the "Wrong Guesses" section. Keep guessing until you complete the word, or you run out of guesses. Completed words will count as a win, whereas running out of guesses will count as a loss. Good luck!</p>
+                    <a href="https://sgenini.github.io/word-guess-game/" target="_blank" rel="noopener noreferrer">Deployed</a> | <a href="https://github.com/sgenini/word-guess-game" target="_blank" rel="noopener noreferrer">GitHub</a>
+                  </MDBCardBody>
+                </MDBCard>
+
+            </MDBCardBody>
             </MDBCard>
           </MDBCol>
         </MDBRow>
