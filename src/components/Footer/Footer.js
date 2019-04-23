581a994bd7cdd4e7b9df1e381d3f145a27c87902
Stephane Genini <sgenini@gmail.com>
2019-04-22 23:17:33 -0700

    rel='noopener noreferrer' for footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,12 +7,9 @@ const Footer = () => {
       <div className="footer-copyright text-center py-3">
         <MDBContainer fluid>
         <p className="text-uppercase mb-0"><strong>&copy; {new Date().getFullYear()} Stéphane Genini</strong></p>
-        {/* eslint-disable-next-line */}
-        <a href="https://github.com/sgenini" target="_blank"> GitHub</a> | 
-        {/* eslint-disable-next-line */}
-        <a href="https://www.linkedin.com/in/stephanegenini/" target="_blank"> LinkedIn</a> | 
-        {/* eslint-disable-next-line */}
-        <a href="https://twitter.com/stephanegenini" target="_blank"> Twitter </a>
+        <a href="https://github.com/sgenini" target="_blank" rel="noopener noreferrer"> GitHub</a> | 
+        <a href="https://www.linkedin.com/in/stephanegenini/" target="_blank" rel="noopener noreferrer"> LinkedIn</a> | 
+        <a href="https://twitter.com/stephanegenini" target="_blank" rel="noopener noreferrer"> Twitter </a>
         <p className="mb-0"><small><span role="img" aria-label="laptop">💻</span> This site was hand-coded with <span className="styled">love</span> using React.js <span role="img" aria-label="victory">✌</span></small></p>
         <style dangerouslySetInnerHTML={{__html: `.styled { transition: color 0.25s ease } .styled:hover { color: red }`}} />
         </MDBContainer>
